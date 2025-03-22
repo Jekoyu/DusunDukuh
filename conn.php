6ef6dc74874868e729c294d4e770f2e23d6d3223
jekoyu <jekoyu157@gmail.com>
2025-03-22 16:28:23 +0700

    fix : env

--- conn.php
+++ conn.php
@@ -1,4 +1,8 @@
 <?php
+// conn.php
+
+// Include file config.php untuk memuat variabel lingkungan
+require __DIR__ . '/config.php';
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
