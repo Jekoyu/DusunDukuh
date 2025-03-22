cc7f1bfd32fe807003c602cdddcdb784d9374afa
jekoyu <jekoyu157@gmail.com>
2025-03-22 16:43:40 +0700

    fix: a upload image

--- conn.php
+++ conn.php
@@ -2,7 +2,7 @@
 // conn.php
 
 // Include file config.php untuk memuat variabel lingkungan
-require __DIR__ . '/config.php';
+require_once __DIR__ . '/config.php';
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
@@ -16,4 +16,3 @@ $conn = new mysqli($host, $username, $password, $dbname, $port);
 if ($conn->connect_error) {
     die("❌ Koneksi Gagal: " . $conn->connect_error);
 }
-?>
