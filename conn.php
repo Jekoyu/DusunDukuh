ffa896c68e75d1e665a86c98fe8127db10560e2d
jekoyu <jekoyu157@gmail.com>
2025-03-22 16:17:31 +0700

    fix; enc

--- conn.php
+++ conn.php
@@ -2,11 +2,11 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-$host = "sql109.infinityfree.com";
-$port = "3306";
-$dbname = "if0_38431756_hmsi";
-$username = "if0_38431756";
-$password = "ggoEFfKAMDt";
+$host = getenv('DB_HOST');
+$port = getenv('DB_PORT');
+$dbname = getenv('DB_DATABASE');
+$username = getenv('DB_USERNAME');
+$password = getenv('DB_PASSWORD');
 $conn = new mysqli($host, $username, $password, $dbname, $port);
 
 if ($conn->connect_error) {
