01b3c496e34c00b95b3e35985416caf8fedbe142
Mohmedalfin <mohmedalfin170505@gmail.com>
2025-04-18 22:02:11 +0700

    Update

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>HMSI Home</title>
+    <title>Padusunan Dukuh</title>
 
     <!-- Boostrep -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
@@ -18,7 +18,6 @@
 
     <!-- Swiper CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" />
-
 </head>
 
 <body>
