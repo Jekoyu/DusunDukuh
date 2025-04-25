ccfdd072c26a5bfe4f07cda5be2ee7e27175e3c4
Jekoyu <jekoyu157@gmail.com>
2025-04-25 16:31:50 +0700

    fix: db

--- conn.php
+++ conn.php
@@ -5,11 +5,12 @@ error_reporting(E_ALL);
 
 require_once __DIR__ . '/config.php';
 
-$host = "167.99.79.36" || 'sql112.infinityfree.com';
-$port = 3306;
-$dbname = "hmsi_impact_2025" || "if0_38676309_hmsi";
-$username = "hmsi" || "if0_38676309";
-$password = "hmsiimpact" || "6blJrX3Ojh";
+$host = $_ENV['DB_HOST'];
+$port = $_ENV['DB_PORT'];
+$dbname = $_ENV['DB_DATABASE'];
+$username = $_ENV['DB_USERNAME'];
+$password = $_ENV['DB_PASSWORD'];
+
 
 $conn = new mysqli($host, $username, $password, $dbname, $port);
 
