0c0c32d8e3e99a62c6db8a2328195257b27174eb
Mohmedalfin <mohmedalfin170505@gmail.com>
2025-04-26 13:15:49 +0700

    update

--- post.php
+++ post.php
@@ -228,37 +228,35 @@ $image_url = (!empty($post['image_url']) && file_exists('assets/uploads/' . $pos
 </head>
 
 <body>
-  <!-- Header would go here -->
+  <!-- Navbar -->
   <header id="header" class="header d-flex align-items-center fixed-top">
     <div class="container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
-      <a href="/index.php" class="logo d-flex align-items-center">
-        <h1 class="sitename">Dusun Dukuh</h1>
+      <a href="index.php" class="logo d-flex align-items-center">
+        <h1 class="sitename">Padukuhan Dukuh</h1>
         <span>.</span>
       </a>
-
       <nav id="navmenu" class="navmenu">
         <ul>
-          <li><a href="/ndex.php">Home</a></li>
-          <li><a href="/about.php">About</a></li>
-          <li><a href="/berita.php" class="active">Berita</a></li>
-
+          <li><a href="index.php">Home</a></li>
+          <li><a href="about.php">Tentang</a></li>
+          <li><a href="berita.php" class="active">Berita</a></li>
           <li class="dropdown">
-            <a href="#"><span>Pejabat</span>
-              <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+            <a href="#"><span>Organisasi Desa</span><i class="bi bi-chevron-down toggle-dropdown"></i></a>
             <ul>
-              <li><a href="/Kel-Tani.php">Kel. Tani</a></li>
-              <li><a href="#">Kel. Maggot</a></li>
-              <li><a href="#">Kel. Pemuda</a></li>
-              <li><a href="#">PKK</a></li>
-              <li><a href="#">LPMKAL</a></li>
+              <li><a href="Kel-Tani.php">Kel. Tani</a></li>
+              <li><a href="Kel-Maggot.php">Kel. Maggot</a></li>
+              <li><a href="Kel-Pemuda.php">Kel. Pemuda</a></li>
+              <li><a href="PKK.php">PKK</a></li>
+              <li><a href="LPMKAL.php">LPMKAL</a></li>
             </ul>
           </li>
-          <li><a href="/grografis.php">Geografis</a></li>
+          <li><a href="grografis.php">Geografis</a></li>
         </ul>
         <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
       </nav>
     </div>
   </header>
+
   <div class="container py-5 mt-4">
     <div class="row">
       <!-- Main Content Column -->
